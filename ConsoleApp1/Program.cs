8e217b478eeff347638ce7a587e095f152b034fb
PAUL STANLEY <paul.stanley@cabbytech.com>
2017-09-16 10:56:18 +0100

    console

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(1);
+            Console.WriteLine(2);
+            Console.WriteLine(3);
+            Console.WriteLine(4);
+
+
+
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
